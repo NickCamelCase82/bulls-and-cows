4b868b2a71dc80607d98f1750f62750cca51db8d
Nikolay Egorenkov <95992893+NickCamelCase82@users.noreply.github.com>
2026-03-29 16:18:05 -0700

    BC-4-jwt-auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  include ExceptionHandler
+
+  before_action :authenticate_request
+
+  private
+
+  def authenticate_request
+    token = request.headers['Authorization']&.split(' ')&.last
+    raise ExceptionHandler::MissingToken, 'Missing token' unless token
+    @current_user_id = JwtService.decode(token)[:user_id]
+  end
 end
